cffa6875f5f9149fd5729cf2e18a592381420b63
仲达超 <sun0821vip@163.com>
2017-07-17 23:43:35 +0800

    add sln

--- FLV_Parse/main.cpp
+++ FLV_Parse/main.cpp
@@ -4,15 +4,9 @@ int main()
 {
 	CFLVFile flvFile;
 	bool bRes = flvFile.LoadFile("../testfile/flv_test.flv");
-	//assert(bRes);
+	assert(bRes);
 
-	//flvFile.Display();
-
-	// 起始dStart~dEnd的音视频保存文件;
-	double dStart = 0.8;
-	double dEnd = 0.9;
-	bRes = flvFile.SaveFile("../testfile/flv_out.flv", dStart, dEnd);
-	//assert(bRes);
+	flvFile.Display();
 
 	return 1;
 }
